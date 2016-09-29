15d591139857156692b08280159bb2dd99e37614
tvand7093 <tyler.j.vanderhoef@gmail.com>
2016-09-29 07:37:04 -0700

    Added docusign tests.

--- Geo-Location-Experiment/Controllers/HomeController.cs
+++ Geo-Location-Experiment/Controllers/HomeController.cs
@@ -1,16 +1,87 @@
-﻿using System;
+﻿using AgencyRM.SDK.DocuSign;
+using AgencyRM.SDK.DocuSign.Notifications;
+using DocuSign.GeneratedClasses.ScopeForm;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace Geo_Location_Experiment.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
+        private class Pop : BasePopulator
+        {
+            public Pop()
+            {
+
+            }
+
+            public override void PopulateFields()
+            {
+                //no fields to populate, so just do nothing.
+            }
+
+            public override void PopulateMap()
+            {
+                //no fields to populate, so just do nothing.
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> SendDoc()
+        {
+            IDocumentConfig config = SignatureFactory.NewConfig();
+            config.IntegratorKey = ConfigurationManager.AppSettings["DocuSign:IntegratorKey"];
+            config.Username = ConfigurationManager.AppSettings["DocuSign:Username"];
+            config.Password = ConfigurationManager.AppSettings["DocuSign:Password"];
+            config.Url = ConfigurationManager.AppSettings["DocuSign:Url"];
+
+            ISignatureRequest request = SignatureFactory.NewRequest();
+            request.NotifyCompleteUrl = "http://geolocation-experimental.trafficmanager.net/Home/SaveRequest";
+            request.TemplateId = ConfigurationManager.AppSettings["DocuSign:TemplateId"];
+
+            var agent = SignatureFactory.NewUser();
+            agent.Email = "tyler.vanderhoef@agencyrm.com";
+            agent.Name = "Tyler Agent";
+            agent.RoleName = AgentSigner.RoleName;
+            agent.Id = "1234";
+
+            var customer = SignatureFactory.NewUser();
+            customer.Email = "tyler.vanderhoef@agencyrm.com";
+            customer.Name = "Tyler Customer";
+            customer.RoleName = BeneficiarySigner.RoleName;
+            customer.Id = "1235";
+
+            List<ISignatureUser> signers = new List<ISignatureUser>() { agent, customer };
+
+            //request the signature
+            var service = SignatureFactory.NewManager(config);
+            service.ChangeSender(config.Username, config.Password);
+            ISignatureResult result = await service.RequestEmailBasedSignature(request, new Pop());
+
+            return View(result.EnvelopeId);
+        }
+
+
+        public void SaveRequest(ApiNotification notification)
+        {
+            if(notification != null)
+            {
+                System.IO.File.WriteAllText("Notification.json", JsonConvert.SerializeObject(notification));
+            }
+            else
+            {
+                //no notification mapped, so just dump the resposne
+                System.IO.File.WriteAllText("Notification.json", JsonConvert.SerializeObject(Request));
+            }
         }
     }
 }
